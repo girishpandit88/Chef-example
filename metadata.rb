80c2643e1265a90efd4a2c01079f0af499110d28
girish pandit <girschol7@gmail.com>
2014-04-02 10:20:44 -0700

    Comment out curl recipe in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'jetty', '~> 0.1.2'
-depends 'curl'
+# depends 'curl'
 depends 'python'
 # depends 'aws-cli'
