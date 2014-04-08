0ebd6d5aa3a18aa8d39310e01a145ef03f18b898
girish pandit <girschol7@gmail.com>
2014-04-08 08:12:04 -0700

    Remove jetty and python dependency

--- myface/metadata.rb
+++ myface/metadata.rb
@@ -6,9 +6,9 @@ description      'Installs/Configures myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'jetty'
+# depends 'jetty'
 depends 'curl'
-depends 'python'
+# depends 'python'
 # depends 'aws-cli'
 depends 's3_file'
 depends 'chef-android-sdk'
